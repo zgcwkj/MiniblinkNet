159d38a837824dfc2fceeb555e488aa4bd74e6eb
zgcwkj <zgcwkj@139.com>
2024-04-12 17:51:31 +0800

    基础示例事件调用

--- src/MiniblinkDemo/Main.cs
+++ src/MiniblinkDemo/Main.cs
@@ -11,16 +11,34 @@ namespace MiniblinkDemo
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.MinimumSize = new System.Drawing.Size(200, 50);
             this.Text = "Main";
+            this.NoneBorderResize = true;
             //
             MiniblinkSetting.EnableHighDPISupport();
+            Border.Set(this, 1, "#B7558E", "#CBCBCB");
             View.ResourceLoader.Add(new EmbedLoader(typeof(Main).Assembly, "wwwroot", "loc.res"));
             View.LoadUri("http://loc.res/index.html");
-            //View.LoadUri("http://zgcwkj.cn");
-
-            View.ShowDevTools();
         }
 
+        [JsFunc]
+        private void ShowWindow()
+        {
+            var main = new Main();
+            main.Show();
+        }
 
+        [JsFunc]
+        private void ShowDialogWindow()
+        {
+            var main = new Main();
+            main.ShowDialog();
+        }
+
+        [JsFunc]
+        private void ShowDevTools()
+        {
+            View.ShowDevTools();
+        }
     }
 }
